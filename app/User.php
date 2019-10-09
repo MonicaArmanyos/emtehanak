ff5b083f6f86d8eb61a9288bde40daf6bc62c10b
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-09 05:11:33 +0200

    test deploy

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name', 'email'
     ];
 
     /**
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        
     ];
 
     /**
