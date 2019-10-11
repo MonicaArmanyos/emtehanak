fc07eeae8e39eea770dcf772b7dcef0c26bb6260
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-11 23:51:19 +0200

    appspec edited

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,10 +1,11 @@
 
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
-sudo chmod -R 0777 /var/www/html/storage
-sudo chmod -R 0777 /var/www/html/bootstrap/cache
+sudo chmod -R 0777 /var/www/html/emtehanak/storage
+sudo chmod -R 0777 /var/www/html/emtehanak/bootstrap/cache
 sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/local/bin/composer
 sudo ln -s /usr/local/bin/composer /usr/bin/composer
-cd /var/www/html
-sudo composer install
+sudo chown -R $USER /var/www/html/emtehanak
+cd /var/www/html/emtehanak
+composer install
