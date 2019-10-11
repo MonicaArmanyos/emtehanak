18785b9fbea0d93c7061a6a0511457d29cdbd164
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-11 23:13:25 +0200

    edit install_composer_dependencies script

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 0777 /var/www/html/storage
 sudo chmod -R 0777 /var/www/html/bootstrap/cache
 cd /var/www/html
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php composer.phar install
