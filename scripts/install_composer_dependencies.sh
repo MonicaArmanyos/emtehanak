58f1d78d9fccf34adf49e2161b450cef5e5962b3
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-12 23:27:58 +0200

    create levels table

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -6,6 +6,6 @@ sudo chmod -R 0777 /var/www/html/bootstrap/cache
 sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/local/bin/composer
 sudo ln -s /usr/local/bin/composer /usr/bin/composer
-sudo chown -R $USER /var/www/html #bdal webapp
+sudo chown $USER /var/www/html -R #bdal webapp
 cd /var/www/html
 composer install
