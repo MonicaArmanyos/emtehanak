c8f585f03153583b834ef88b4344c6d7bbf41650
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-13 03:47:40 +0200

    editing scripts

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,12 +1,11 @@
 
 #!/bin/bash
-# Set permissions to storage and bootstrap cache
 sudo chmod -R 0777 /var/www/html/storage
 sudo chmod -R 0777 /var/www/html/bootstrap/cache
 sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/local/bin/composer
 sudo ln -s /usr/local/bin/composer /usr/bin/composer
-sudo chown -R $USER:$USER /var/www/html/ #bdal webapp
+sudo chown -R $USER:$USER /var/www/html/ 
 cd /var/www/html
 composer install
 
