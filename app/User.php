2ae5b533d7d5d17b42ac6b1c0cf824cd1bdb4419
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-14 22:48:43 +0200

    test codedeploy

--- app/User.php
+++ app/User.php
@@ -10,12 +10,4 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email'
-    ];
 }
