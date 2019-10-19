a76dd54989232501356aae271838efff1bb2f342
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-19 03:44:50 +0200

    test deloy

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -4,7 +4,7 @@ sudo chmod -R 0777 /var/www/html/bootstrap/cache
 sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/local/bin/composer
 sudo ln -s /usr/local/bin/composer /usr/bin/composer
-sudo chown -R ec2-user:ec2-user /var/www/html/ 
+# sudo chown -R ec2-user:ec2-user /var/www/html/ 
 cd /var/www/html
 composer install
 cp .env.example  .env
