936747b60e3fecf533b6d12e9414fda8c53e20ab
MonicaArmanyos <monicamagdy70@gmail.com>
2019-10-19 05:15:00 +0200

    test migration

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -9,6 +9,3 @@ cd /var/www/html
 composer install
 cp .env.example  .env
 php artisan key:generate
-source ~/.bashrc
-php artisan migrate
-# php artisan db:seed 
--- scripts/migrate_db.sh
+++ scripts/migrate_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 # Run artisan commands
+source ~/.bashrc
 php artisan migrate
 php artisan db:seed 
 
