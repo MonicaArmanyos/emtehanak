2b2d525040b0d033e6ace8ec1ed33f4335ea80b3
Monica Magdy <monica.magdy@ibtikar.net.sa>
2019-10-22 20:39:53 +0200

    getting exam time in progress

--- app/Http/Controllers/V1/SubjectController.php
+++ app/Http/Controllers/V1/SubjectController.php
@@ -11,6 +11,10 @@ class SubjectController extends Controller
     public function index($student_id)
     {
         $student = Student::findOrFail($student_id);
-        $subjects = $student->subjects;
+        $level_id =$student->level->id;
+        $student = $student->with(['subjects.levels'=>function($query) use ($level_id){
+            $query->where("levels.id",$level_id);
+        }])->first();
+       dd($student->subjects->last()->levels->first()->pivot->exam_time);
     }
 }
